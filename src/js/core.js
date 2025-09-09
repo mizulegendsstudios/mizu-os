a5138d37aafdbeae3ee8d127bac67ca88387619b
Moises Núñez Ibarra <mizulegendsgg@gmail.com>
2025-09-09 01:06:35 +0000

    Create core.js

---
+++ src/js/core.js
@@ -0,0 +1,20 @@
+/**
+ * @fileoverview Archivo principal que orquesta la lógica de la aplicación.
+ * @author Gemini
+ */
+//STABLE
+//import { initializeNodeManager } from "./dev/node_manager.js"; // Importa el nuevo módulo
+
+/**
+ * Versión de la aplicación.
+ * @constant {string}
+ */
+const VERSION = '2.10.01';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Registra la versión de la aplicación en la consola.
+    console.log(`Aplicación Mizulegends iniciada. Versión: ${VERSION}`);
+
+    // Inicializa todos los módulos.
+    //initializeNodeManager(); // Se inicializa el módulo de gestión de nodos.
+});
