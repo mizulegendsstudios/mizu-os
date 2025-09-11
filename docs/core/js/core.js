b5df398ab209aeb82b81a4b8be4e3889e4b08aa0
Moises Núñez Ibarra <mizulegendsgg@gmail.com>
2025-09-11 08:32:38 +0000

    Update core.js

--- docs/core/js/core.js
+++ docs/core/js/core.js
@@ -3,7 +3,7 @@
      */
 
     // Versión de la aplicación.
-const VERSION = '2.10.13';
+const VERSION = '2.10.14';
     // Versión de la aplicación impreso en log.
 console.log(`Aplicación Mizu OS iniciada. Versión: ${VERSION}`);
 
@@ -18,7 +18,7 @@ console.log(`Cargando sistema mejoras en desarrollo...`);
     // import { initializeZoomAndPan } from "./docs/core/js/zoom.js"; // Dejado comentado por compatibilidad con diagram
 //import { drawLines } from './docs/apps/diagram/js/drawlines.js';
     //Importamos initDiagram desde nodos.js (sistema de nodos tradicionales)
-import { initDiagram } from './docs/apps/diagram/js/nodos.js';
+import { initDiagram } from '/docs/apps/diagram/js/nodos.js';
 
     // Importamos createContainerWithPorts desde dev/nodos-puertos.js (nuevo sistema de puertos anclados)
 //import { createContainerWithPorts } from './docs/apps/diagram/js/nodos-puertos.js';
