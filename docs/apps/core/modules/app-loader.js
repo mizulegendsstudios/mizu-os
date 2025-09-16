544792ece0b9a695c955a2e7fc67be3348c5bde6
Moises Núñez Ibarra <mizulegendsgg@gmail.com>
2025-09-16 09:50:22 +0000

    Update app-loader.js

--- docs/apps/core/modules/app-loader.js
+++ docs/apps/core/modules/app-loader.js
@@ -1,6 +1,6 @@
 /*
  * Mizu OS - App Loader Module
- * Copyright (C) 2025 Mizu Legends Studios
+ * Copyright (C) 2025 Mizu Legends Studios.
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
